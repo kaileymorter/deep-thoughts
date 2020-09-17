50b163843fab01ea91d80cbf84a211a4097067f2
Spartacus <k_morter@outlook.com>
2020-09-16 21:00:34 -0600

    edited config file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
+connection = `mongodb+srv://kaileymorter:${process.env.DB_PASS}}@school-cluster.5ae8t.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
 mongoose.connect(
-  process.env.MONGODB_URI || `mongodb+srv://kaileymorter:${process.env.DB_PASS}}@school-cluster.5ae8t.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, 
+  connection, 
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
       useFindAndModify: false
     }
+    .then(() => console.log("Database Connected Successfully"))
+    .catch(err => console.log(err));
 );
 
 module.exports = mongoose.connection;
