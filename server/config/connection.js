5cdee0c10bcbd18d0994e7dadfd8f893a7b65105
Spartacus <k_morter@outlook.com>
2020-09-16 20:47:17 -0600

    edited config

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 mongoose.connect(
-  process.env.MONGODB_URI || `mongodb+srv://kaileymorter:${process.env.DB_PASS}@cluster0.5ae8t.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, 
+  process.env.MONGODB_URI || `mongodb+srv://kaileymorter:${process.env.DB_PASS}}@school-cluster.5ae8t.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, 
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
